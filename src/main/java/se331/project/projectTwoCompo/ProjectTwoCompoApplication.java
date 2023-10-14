e0ee12a90f14b60cf7657b76d4ffe68e7f76f95d
NOHP404 <poopa_somth@cmu.ac.th>
2023-10-14 21:06:46 +0700

    init commit

---
+++ src/main/java/se331/project/projectTwoCompo/ProjectTwoCompoApplication.java
@@ -0,0 +1,13 @@
+package se331.project.projectTwoCompo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTwoCompoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTwoCompoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se331/project/projectTwoCompo/ProjectTwoCompoApplicationTests.java
@@ -0,0 +1,13 @@
+package se331.project.projectTwoCompo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTwoCompoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
