e749cb6ad1a895ef062856a1f501adc364c90dbf
GIgaSora <krittayoch_jo@cmu.ac.th>
2023-10-17 22:09:05 +0700

    Bucket Controller~

---
+++ src/main/java/se331/project/projectTwoCompo/controller/BucketController.java
@@ -0,0 +1,32 @@
+package se331.project.projectTwoCompo.controller;
+
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.ServletException;
+import lombok.RequiredArgsConstructor;
+import se331.project.projectTwoCompo.util.CloudStorageHelper;
+
+@Controller
+@RequiredArgsConstructor
+public class BucketController {
+    final CloudStorageHelper cloudStorangHelper;
+
+    @PostMapping("/uploadFile")
+    public ResponseEntity<?> uploadFile(@RequestPart(value = "file") MultipartFile file)
+            throws IOException, ServletException {
+        return ResponseEntity.ok(this.cloudStorangHelper.getImageUrl(file, "imgup-23aa6.appspot.com"));
+    }
+
+    @PostMapping("/uploadImage")
+    public ResponseEntity<?> uploadFileComponent(@RequestParam(value = "image") MultipartFile file) throws IOException, ServletException{
+        return
+        ResponseEntity.ok(this.cloudStorangHelper.getStorageFileDto(file, "imgup-23aa6.appspot.com"));
+    }
+}
