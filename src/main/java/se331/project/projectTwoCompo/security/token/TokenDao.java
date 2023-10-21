0b747f8a1e1b2caef075e1382efd2fc1c8cb01da
pipe-lines <changseong_lee@cmu.ac.th>
2023-10-21 19:19:36 +0700

    Security added

---
+++ src/main/java/se331/project/projectTwoCompo/security/token/TokenDao.java
@@ -0,0 +1,5 @@
+package se331.project.projectTwoCompo.security.token;
+
+public interface TokenDao {
+    void save(Token token);
+}
---
+++ src/main/java/se331/project/projectTwoCompo/security/token/TokenType.java
@@ -0,0 +1,5 @@
+package se331.project.projectTwoCompo.security.token;
+
+public enum TokenType {
+  BEARER
+}
